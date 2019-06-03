9bc9b4b2b28be41573857465b0b4c66529ea837b
Sergio Sáez <ssaez@iti.es>
2019-06-03 10:18:05 +0200

    TTS Support

---
+++ runtime/s-ttssup.ads
@@ -0,0 +1,16 @@
+with System.BB.Threads;
+
+package System.TTS_Support is
+
+   pragma Preelaborate (TTS_Support);
+
+   procedure Hold (T : System.BB.Threads.Thread_Id;
+                   Check_Protected_Action : Boolean := False);
+   procedure Continue (T : System.BB.Threads.Thread_Id);
+   function Is_Held (T : System.BB.Threads.Thread_Id)
+                   return Boolean;
+
+   procedure Update_Priority (Thread : System.BB.Threads.Thread_Id;
+                              Priority : Integer);
+
+end System.TTS_Support;
