abc1bf1eb554c102d5b02737204f6345a2254129
Sergio Saez Barona <ssaez@iti.es>
2019-07-29 21:01:26 +0200

    Headers and license files have been added

--- extensions/xada.ads
+++ extensions/xada.ads
@@ -1,4 +1,28 @@
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                   X A D A                                  --
+--                                                                            --
+--                                   S P E C                                  --
+--                                                                            --
+-- @author 2018-19 Jorge Real (jorge@disca.upv.es)                            --
+-- @author 2018-19 Sergio Saez (ssaez@disca.upv.es)                           --
+--                                                                            --
+-- This GNARL extension is free software: you can redistribute it and/or      --
+-- modify it under the terms of the GNU General Public License as published   --
+-- by the Free Software Foundation, either version 3 of the License, or (at   --
+-- your option) any later version.                                            --
+--                                                                            --
+-- This GNARL extension is distributed in the hope that it will be useful,    --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of             --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General  --
+-- Public License for more details.                                           --
+--                                                                            --
+-- You should have received a copy of the GNU General Public License along    --
+-- with this program. If not, see <https://www.gnu.org/licenses/>.            --
+--                                                                            --
+--------------------------------------------------------------------------------
+
 package XAda is
-   pragma Pure (XAda);   
+   pragma Pure (XAda);
 
 end XAda;
