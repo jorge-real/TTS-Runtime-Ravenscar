9c98dec42047110856405a7ec15e6ac83119673a
Sergio Sáez <ssaez@iti.es>
2020-01-08 17:07:31 +0100

    In PA added to TTS support

--- runtime/s-ttssup.adb
+++ runtime/s-ttssup.adb
@@ -8,22 +8,22 @@ package body System.TTS_Support is
 
    procedure Suspend_Thread (Thread : System.BB.Threads.Thread_Id);
    procedure Resume_Thread (Thread : System.BB.Threads.Thread_Id);
-   
+
    ----------
    -- Hold --
    ----------
-   
+
    procedure Hold (T : Thread_Id;
                    Check_Protected_Action : Boolean := False) is
       T_Id : constant Task_Id := To_Task_Id (T.ATCB);
    begin
       if not Is_Held (T) then
-         if not In_Protected_Action(T) then
+         if not In_Protected_Action (T) then
             T_Id.Common.State := Asynchronous_Hold;
             T.Hold_Signaled := False;
             Suspend_Thread (T);
          elsif Check_Protected_Action and then
-           In_Protected_Action(T, 1) 
+           In_Protected_Action (T, 1)
          then
             raise Program_Error with ("Hold requested in a PA");
          else
@@ -31,11 +31,11 @@ package body System.TTS_Support is
          end if;
       end if;
    end Hold;
-   
+
    --------------
    -- Continue --
    --------------
-      
+
    procedure Continue (T : Thread_Id) is
       T_Id : constant Task_Id := To_Task_Id (T.ATCB);
    begin
@@ -53,31 +53,30 @@ package body System.TTS_Support is
    -------------
    -- Is_Held --
    -------------
-   
+
    function Is_Held (T : Thread_Id)
                     return Boolean is
       T_Id : constant Task_Id := To_Task_Id (T.ATCB);
    begin
       return (T_Id.Common.State = Asynchronous_Hold);
    end Is_Held;
-   
+
    -------------------------
    -- In_Protected_Action --
    -------------------------
-   
-   function In_Protected_Action (T : Thread_Id; 
-				 Level : Natural := 0) 
-				return Boolean is
+
+   function In_Protected_Action (T : Thread_Id;
+                                 Level : Natural := 0)
+                                return Boolean is
       T_Id : constant Task_Id := To_Task_Id (T.ATCB);
    begin
       return (T_Id.Common.Protected_Action_Nesting > Level);
-   end In_Protected_Action;  
-   
-   
+   end In_Protected_Action;
+
    -------------------------
    -- Internal procedures --
    -------------------------
-   
+
    procedure Suspend_Thread (Thread : System.BB.Threads.Thread_Id) is
    begin
       Thread.State := Suspended;
@@ -89,6 +88,5 @@ package body System.TTS_Support is
       Thread.State := Runnable;
       Queues.Insert (Thread);
    end Resume_Thread;
-   
-   
+
 end System.TTS_Support;
